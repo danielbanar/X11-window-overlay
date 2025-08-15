9123264d040d3444764033ad8100389acc5cfe2d
Daniel Banar <daniel.banar5@gmail.com>
2025-08-15 16:34:36 +0200

    window tracking

--- overlay.cpp
+++ overlay.cpp
@@ -11,73 +11,145 @@
 #include <X11/extensions/Xcomposite.h>
 #include <X11/extensions/Xfixes.h>
 
-#define ALIGN_LEFT 0
 #define ALIGN_CENTER 1
-#define ALIGN_RIGHT 2
-#define BASIC_EVENT_MASK (StructureNotifyMask|ExposureMask|PropertyChangeMask|EnterWindowMask|LeaveWindowMask|KeyPressMask|KeyReleaseMask|KeymapStateMask)
-#define NOT_PROPAGATE_MASK (KeyPressMask|KeyReleaseMask|ButtonPressMask|ButtonReleaseMask|PointerMotionMask|ButtonMotionMask)
-#define SHAPE_MASK ShapeNotifyMask
+#define BASIC_EVENT_MASK (StructureNotifyMask | ExposureMask | PropertyChangeMask | EnterWindowMask | LeaveWindowMask | KeyPressMask | KeyReleaseMask | KeymapStateMask)
+#define NOT_PROPAGATE_MASK (KeyPressMask | KeyReleaseMask | ButtonPressMask | ButtonReleaseMask | PointerMotionMask | ButtonMotionMask)
 
-// Global X11 variables
 Display *g_display;
 int g_screen;
 Window g_win;
 Colormap g_colormap;
 GC gc;
 XFontStruct *font;
+XColor ltblue, blacka, transparent, white;
 
-// Colors
-XColor red, black, blacka, ltblue, transparent, white;
-
-// Font
 const char *font_name = "9x15bold";
 const int font_width = 9;
 const int font_height = 15;
+int WIDTH = 640;
+int HEIGHT = 480;
+int POSX = 0;
+int POSY = 0;
 
-// Window size
-int WIDTH  = 1920;
-int HEIGHT = 1080;
-
-XColor createXColorFromRGB(short r, short g, short b) {
+XColor createXColorFromRGB(short r, short g, short b)
+{
     XColor color;
-    color.red   = (r * 0xFFFF) / 0xFF;
+    color.red = (r * 0xFFFF) / 0xFF;
     color.green = (g * 0xFFFF) / 0xFF;
-    color.blue  = (b * 0xFFFF) / 0xFF;
+    color.blue = (b * 0xFFFF) / 0xFF;
     color.flags = DoRed | DoGreen | DoBlue;
-    if (!XAllocColor(g_display, DefaultColormap(g_display, g_screen), &color)) {
+    if (!XAllocColor(g_display, DefaultColormap(g_display, g_screen), &color))
+    {
         std::cerr << "Cannot create color" << std::endl;
         exit(1);
     }
     return color;
 }
 
-XColor createXColorFromRGBA(short r, short g, short b, short a) {
+XColor createXColorFromRGBA(short r, short g, short b, short a)
+{
     XColor color = createXColorFromRGB(r, g, b);
     *(&color.pixel) = ((*(&color.pixel)) & 0x00ffffff) | (a << 24);
     return color;
 }
 
-void allow_input_passthrough(Window w) {
+void allow_input_passthrough(Window w)
+{
     XserverRegion region = XFixesCreateRegion(g_display, NULL, 0);
     XFixesSetWindowShapeRegion(g_display, w, ShapeInput, 0, 0, region);
     XFixesDestroyRegion(g_display, region);
 }
+void getAbsoluteGeometry(Window win)
+{
+    XWindowAttributes attr;
+    XGetWindowAttributes(g_display, win, &attr);
 
-void openDisplay() {
-    g_display = XOpenDisplay(0);
-    if (!g_display) {
-        std::cerr << "Failed to open X display" << std::endl;
-        exit(1);
+    int x = 0, y = 0;
+    Window child = win;
+    Window root_return, parent_return;
+    Window *children;
+    unsigned int nchildren;
+
+    // Walk up the hierarchy to root
+    Window current = win;
+    while (true)
+    {
+        XTranslateCoordinates(g_display, current, DefaultRootWindow(g_display), 0, 0, &x, &y, &child);
+
+        if (!XQueryTree(g_display, current, &root_return, &parent_return, &children, &nchildren))
+            break;
+
+        if (children)
+            XFree(children);
+
+        if (parent_return == DefaultRootWindow(g_display))
+            break;
+
+        current = parent_return;
     }
-    g_screen = DefaultScreen(g_display);
-    int shape_event_base, shape_error_base;
-    if (!XShapeQueryExtension(g_display, &shape_event_base, &shape_error_base)) {
-        std::cerr << "NO shape extension!" << std::endl;
-        exit(1);
-    }
+
+    POSX = x;
+    POSY = y;
+    WIDTH = attr.width;
+    HEIGHT = attr.height;
+
+    std::cout << "Absolute position: " << POSX << "," << POSY << " size " << WIDTH << "x" << HEIGHT << "\n";
 }
 
-void createShapedWindow() {
+bool findWindowByName(Window root, const std::string &name, Window &outWin)
+{
+    Window root_return, parent_return;
+    Window *children;
+    unsigned int nchildren;
+    if (XQueryTree(g_display, root, &root_return, &parent_return, &children, &nchildren))
+    {
+        for (unsigned int i = 0; i < nchildren; i++)
+        {
+            char *window_name = nullptr;
+            if (XFetchName(g_display, children[i], &window_name) && window_name)
+            {
+                std::string wname(window_name);
+                XFree(window_name);
+                if (wname.find(name) != std::string::npos)
+                {
+                    outWin = children[i];
+                    if (children)
+                        XFree(children);
+                    return true;
+                }
+            }
+            if (findWindowByName(children[i], name, outWin))
+            {
+                if (children)
+                    XFree(children);
+                return true;
+            }
+        }
+        if (children)
+            XFree(children);
+    }
+    return false;
+}
+
+void getWindowGeometry(Window win)
+{
+    XWindowAttributes attr;
+    XGetWindowAttributes(g_display, win, &attr);
+
+    Window child;
+    int x, y;
+    XTranslateCoordinates(g_display, win, DefaultRootWindow(g_display), 0, 0, &x, &y, &child);
+
+    POSX = x;
+    POSY = y;
+    WIDTH = attr.width;
+    HEIGHT = attr.height;
+
+    std::cout << "Found target window at (" << POSX << "," << POSY << "), size " << WIDTH << "x" << HEIGHT << "\n";
+}
+
+void createOverlayWindow()
+{
     XColor bgcolor = createXColorFromRGBA(0, 0, 0, 0);
     XVisualInfo vinfo;
     XMatchVisualInfo(g_display, DefaultScreen(g_display), 32, TrueColor, &vinfo);
@@ -95,10 +167,10 @@ void createShapedWindow() {
     attr.override_redirect = 1;
     attr.colormap = g_colormap;
 
-    unsigned long mask = CWColormap | CWBorderPixel | CWBackPixel | CWEventMask | CWWinGravity |
-                         CWBitGravity | CWSaveUnder | CWDontPropagate | CWOverrideRedirect;
+    unsigned long mask = CWColormap | CWBorderPixel | CWBackPixel | CWEventMask |
+                         CWWinGravity | CWBitGravity | CWSaveUnder | CWDontPropagate | CWOverrideRedirect;
 
-    g_win = XCreateWindow(g_display, DefaultRootWindow(g_display), 0, 0, WIDTH, HEIGHT, 0,
+    g_win = XCreateWindow(g_display, DefaultRootWindow(g_display), POSX, POSY, WIDTH, HEIGHT, 0,
                           vinfo.depth, InputOutput, vinfo.visual, mask, &attr);
 
     XShapeCombineMask(g_display, g_win, ShapeInput, 0, 0, None, ShapeSet);
@@ -106,54 +178,79 @@ void createShapedWindow() {
     XMapWindow(g_display, g_win);
 }
 
-void initOverlay() {
-    openDisplay();
-    createShapedWindow();
+void initOverlay()
+{
+    g_screen = DefaultScreen(g_display);
+    createOverlayWindow();
 
     gc = XCreateGC(g_display, g_win, 0, 0);
     font = XLoadQueryFont(g_display, font_name);
-    if (!font) {
+    if (!font)
+    {
         std::cerr << "Unable to load font " << font_name << std::endl;
         font = XLoadQueryFont(g_display, "fixed");
     }
 
-    red   = createXColorFromRGBA(255, 0, 0, 255);
     ltblue = createXColorFromRGBA(0, 255, 255, 255);
-    black = createXColorFromRGBA(0, 0, 0, 255);
     blacka = createXColorFromRGBA(0, 0, 0, 150);
     white = createXColorFromRGBA(255, 255, 255, 255);
     transparent = createXColorFromRGBA(255, 255, 255, 0);
 }
 
-void drawString(const char *text, int x, int y, XColor fg, XColor bg, int align) {
+void drawString(const char *text, int x, int y, XColor fg, XColor bg, int align)
+{
     int tlen = strlen(text);
     XSetFont(g_display, gc, font->fid);
-    if (bg.pixel != transparent.pixel) {
+    if (bg.pixel != transparent.pixel)
+    {
         XSetForeground(g_display, gc, bg.pixel);
         XFillRectangle(g_display, g_win, gc,
-                       x - (align == ALIGN_CENTER ? tlen*font_width/2 : (align == ALIGN_RIGHT ? tlen*font_width : 0)) - 4,
-                       y, tlen*font_width + 8, font_height + 4);
+                       x - (align == ALIGN_CENTER ? tlen * font_width / 2 : 0) - 4,
+                       y, tlen * font_width + 8, font_height + 4);
     }
     XSetForeground(g_display, gc, fg.pixel);
     XDrawString(g_display, g_win, gc,
-                x - (align == ALIGN_CENTER ? tlen*font_width/2 : (align == ALIGN_RIGHT ? tlen*font_width : 0)),
+                x - (align == ALIGN_CENTER ? tlen * font_width / 2 : 0),
                 y + font_height, text, tlen);
 }
 
-int main() {
+int main()
+{
+    g_display = XOpenDisplay(0);
+    if (!g_display)
+    {
+        std::cerr << "Failed to open X display" << std::endl;
+        return 1;
+    }
+
+    Window target;
+    if (!findWindowByName(DefaultRootWindow(g_display), "gst-launch-1.0", target))
+    {
+        std::cerr << "Could not find window with name containing 'gst-launch-1.0'\n";
+        return 1;
+    }
+
+    getAbsoluteGeometry(target);
     initOverlay();
 
     auto start_time = std::chrono::steady_clock::now();
 
-    while (true) {
+    while (true)
+    {
+        // Recalculate GStreamer window position & size
+        getAbsoluteGeometry(target);
+
+        // Move & resize overlay if needed
+        XMoveResizeWindow(g_display, g_win, POSX, POSY, WIDTH, HEIGHT);
+
+        // Draw time since start
         auto now = std::chrono::steady_clock::now();
         auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time).count();
 
         std::string text = std::to_string(elapsed) + " ms";
-        drawString(text.c_str(), WIDTH/2, HEIGHT/2, ltblue, blacka, ALIGN_CENTER);
+        drawString(text.c_str(), WIDTH / 2, HEIGHT / 2, ltblue, blacka, ALIGN_CENTER);
 
         XFlush(g_display);
-
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 }
